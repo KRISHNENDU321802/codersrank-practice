17929b06605e7fca5990bb8d2374bdca61663c05
KRISHNENDU321802 <krishnendupatra627@gmail.com>
2020-02-25 00:03:11 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // Start with the index.html and start editing.
-
-import './style.css';
+<html>
+<head>
+<title>Page title</title></head>
+<body>
+ <h1>My Second Heading</h1><br>
+ <p>My first paragraph</p>
+ <p>My second paragraph</p>
+</body>
+</html>
